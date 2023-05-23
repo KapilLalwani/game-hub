83e7992ad3fc4143990ad2c46d6ede8dce34de02
KapilLalwani <lalwanikapil001@gmail.com>
2023-05-23 17:48:53 +0100

    Adding GameContainer to remove duplicated styling

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,8 +2,8 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width={300}>
-      <Skeleton height={"200px"} />
+    <Card>
+      <Skeleton height={200} />
       <CardBody>
         <SkeletonText />
       </CardBody>
